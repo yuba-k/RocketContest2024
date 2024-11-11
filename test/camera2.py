bd8fd21f80af29f1f0769bb671229ce0fa0f0350
yubak <yubakei062318@outlook.jp>
2024-11-11 18:01:21 +0900

    picamera2を用いたpicamera制御

---
+++ test/camera2.py
@@ -0,0 +1,19 @@
+from picamera2 import Picamera2
+import cv2
+class Camera():
+    def __init__(self):
+        self.picam = Picamera2()
+        self.picam.configure(self.picam.create_still_configuration(main={"format":"RGB888","size":(640,480)}))
+    def cap(self):
+        self.picam.start()
+        im = self.picam.capture_array()
+        self.save(im)
+    def save(self,im):
+        cv2.imwrite("test_cv2.jpg",im)
+
+def main():
+    camera = Camera()
+    camera.cap()
+
+if __name__ == "__main__":
+    main()
