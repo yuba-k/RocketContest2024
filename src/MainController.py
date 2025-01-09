24178064a983b0701edd1849ff777246af4ead14
yubak <yubakei062318@outlook.jp>
2025-01-09 17:18:24 +0900

    本番用プログラム-未試験実装中

--- src/MainController.py
+++ src/MainController.py
@@ -1,9 +1,13 @@
 import start
 import logwrite
 import gps
+import img_dtc as imgDetection
+import WeakFMEmitter
 
+import time
 from enum import Enum
 
+# 状態制御クラス
 class Status(Enum):
     WAIT = "waiting_start"
     START = "start"
@@ -12,19 +16,35 @@ class Status(Enum):
     GOAL = "GOAL"
 
 log = logwrite.MyLogging()
+fm = WeakFMEmitter.FMemitter()
 start.init()
 gps.init()
 
 def main():
-    condition = Status.WAIT
-    log.write(condition,"INFO")
-    start.awaiting()
+    try:
+        fm.stringToAscii("taikityu-")
+        condition = Status.WAIT
+        log.write(condition,"INFO")
+        start.awaiting()
 
-    condition = Status.START
-    log.write(condition,"INFO")
+        condition = Status.START
+        fm.stringToAscii("ugokidasuyo-")
+        log.write(condition,"INFO")
 
-    condition = Status.LOCATIONPhase
-    gps.main()
+        start_time = time.time()
+        finish_time = time.time() + (18 * 60)#強制終了
+
+        condition = Status.LOCATIONPhase
+        gps.main()
+
+        condition = Status.CAMERAPhase
+        log.write(condition,"INFO")
+        imgDetection.main()
+        log.write(condition,"INFO")
+
+        fm.stringToAscii("go-rusimasita")
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
     
 if __name__ =="__main__":
     main()
