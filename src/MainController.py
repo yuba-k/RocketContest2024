1ab15e8bc495fe14574aa020760a3bc2ac0c9666
yubak <yubakei062318@outlook.jp>
2025-01-10 17:47:07 +0900

    強制終了実装

--- src/MainController.py
+++ src/MainController.py
@@ -11,7 +11,7 @@ import thread
 
 # 状態制御クラス
 class Status(Enum):
-    WAIT = "waiting_start"
+    WAIT = "waiting"
     START = "start"
     LOCATIONPhase = "search_with_GPS"
     CAMERAPhase = "search_with_CAMERA"
@@ -22,39 +22,49 @@ fm = WeakFMEmitter.FMemitter()
 start.init()
 gps.init()
 
+flag = False
+
 def stop_reqest():
+    global flag
     finish_time = time.time() + (18 * 60)#強制終了
     while True:
         if(time.time()>=finish_time):
+            flag = True
             thread.interrupt_main()
         time.sleep(1)
 
 def main():
     try:
         fm.stringToAscii("taikityu-")
-        condition = Status.WAIT
+        condition = Status.WAIT.value
         log.write(condition,"INFO")
         start.awaiting()
 
-        condition = Status.START
+        thread.start_new_thread(stop_reqest, ())# 強制終了指示を待機
+
+        condition = Status.START.value
         fm.stringToAscii("ugokidasuyo-")
         log.write(condition,"INFO")
 
-        thread.start_new_thread(stop_reqest, ())# 強制終了指示を待機
-
-        condition = Status.LOCATIONPhase
+        condition = Status.LOCATIONPhase.value
         gps.main()
 
-        condition = Status.CAMERAPhase
+        condition = Status.CAMERAPhase.value
         log.write(condition,"INFO")
         img_dtc.main()
+        condition = Status.GOAL.value
         log.write(condition,"INFO")
 
         fm.stringToAscii("go-rusimasita")
     except KeyboardInterrupt:
-        print("KeyboardInterrupt")
+        if flag:
+            log.write("強制終了","INFO")
+        else:
+            print("KeyboardInterrupt")
     except SystemExit:
         printf("強制終了させました")
+    finally:
+        pass
     
 if __name__ =="__main__":
     main()
