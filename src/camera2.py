4b773484572f164063349f70fa6d6a1ba6f5b680
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-02-28 21:37:37 +0900

    例外処理の追加

--- src/camera2.py
+++ src/camera2.py
@@ -7,21 +7,31 @@ import logwrite
 
 class Camera():
     def __init__(self):
-        config = configloading.Config_reader()
-        height = config.reader("camera","height","intenger")
-        weight = config.reader("camera","weight","intenger")
-        log = logwrite.MyLogging()
-        self.picam = Picamera2()
-        self.picam.configure(self.picam.create_still_configuration(main={"format":"RGB888","size":(weight,height)}))
+        try:
+            config = configloading.Config_reader()
+            height = config.reader("camera","height","intenger")
+            weight = config.reader("camera","weight","intenger")
+            self.log = logwrite.MyLogging()
+            self.picam = Picamera2()
+            self.picam.configure(self.picam.create_still_configuration(main={"format":"RGB888","size":(weight,height)}))
+        except Exception as e:
+            self.log.write("初期化時にエラーが発生","ERROR")
+            raise
     def cap(self,cnt):
-        self.picam.start()
-        im = self.picam.capture_array()
-        im = np.flipud(im)
-        im = np.fliplr(im)
-        self.save(im,cnt)
-        return im
+        try:
+            self.picam.start()
+            im = self.picam.capture_array()
+            im = np.flipud(im)
+            im = np.fliplr(im)
+            self.save(im,cnt)
+            return im
+        except Exception as e:
+            self.log.write("撮影時にエラーが発生","ERROR")
+            return None
     def save(self,im,cnt):
         cv2.imwrite(f"../img/default/{cnt}test_cv2.jpg",im)
+    def disconnect(self):
+        self.picam.close()
 
 def main():
     camera = Camera()
